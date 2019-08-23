157bd2941338491f5cc05bd605f324cca56c1fe0
Chong.Zhang <woodyzc@gmail.com>
2019-08-23 16:51:49 -0400

    add 1-b

--- code.R
+++ code.R
@@ -4,3 +4,11 @@ y = (1-(1-exp(-t))*(1-exp(-t/2)))*exp(-t)+(1-(1-exp(-t)*exp(-2*t))*(1-exp(-t/2)*
 
 plot(x=t,y=y,xlab = "time",ylab ="probability")
 title(main = "probability of system works")
+
+t=1/2
+h1=(1-(1-exp(-t))*(1-exp(-t/2)))*exp(-t)
+p_5 = exp(-t)
+y = (1-(1-exp(-t))*(1-exp(-t/2)))*exp(-t)+(1-(1-exp(-t)*exp(-2*t))*(1-exp(-t/2)*exp(-t/3)))*(1-exp(-t))
+
+
+x = h1*p_5/y
